a3b169dc507bf7186c9d6505b921511f16a2af49
Axyoan Marcelo Castillo <axyoan.marcelo@alumnos.udg.mx>
2021-03-17 15:37:09 -0600

    Title cambiado

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ catch (Exception $e) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP intro</title>
+    <title>PHP introducción</title>
 </head>
 <body>
     <h1>Manejador de Tareas</h1>
